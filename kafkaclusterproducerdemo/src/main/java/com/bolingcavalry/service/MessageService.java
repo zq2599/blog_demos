e0415730b0b405c4dcce0d85c2eb81d5a601b77f
zq2599 <zq2599@gmail.com>
2017-10-28 10:02:22 +0800

    add kafka cluster producer demo

---
+++ kafkaclusterproducerdemo/src/main/java/com/bolingcavalry/service/MessageService.java
@@ -0,0 +1,16 @@
+package com.bolingcavalry.service;
+
+/**
+ * @author willzhao
+ * @version V1.0
+ * @Description: 发送消息的服务接口
+ * @email zq2599@gmail.com
+ * @Date 2017/10/28 上午9:57
+ */
+public interface MessageService {
+    /**
+     * 发送一个普通的消息
+     * @param message
+     */
+    void sendSimpleMsg(String message);
+}
---
+++ kafkaclusterproducerdemo/src/main/java/com/bolingcavalry/service/impl/MessageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bolingcavalry.service.impl;
+
+import com.bolingcavalry.service.MessageService;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author willzhao
+ * @version V1.0
+ * @Description: 实现消息服务
+ * @email zq2599@gmail.com
+ * @Date 2017/10/28 上午9:58
+ */
+public class MessageServiceImpl implements MessageService{
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+
+    public void sendSimpleMsg(String message) {
+
+    }
+}
