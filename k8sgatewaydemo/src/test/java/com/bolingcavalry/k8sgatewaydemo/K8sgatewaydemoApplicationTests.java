da532ef4670c38ee1f5b6b0fbf60c733e5428143
zq2599 <zq2599@gmail.com>
2019-07-07 20:21:16 +0800

    增加k8s环境下验证springcloud gateway的demo

---
+++ k8sgatewaydemo/src/test/java/com/bolingcavalry/k8sgatewaydemo/K8sgatewaydemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bolingcavalry.k8sgatewaydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sgatewaydemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ webdemo/src/main/java/com/bolingcavalry/webdemo/WebdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bolingcavalry.webdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdemoApplication.class, args);
+    }
+
+}
