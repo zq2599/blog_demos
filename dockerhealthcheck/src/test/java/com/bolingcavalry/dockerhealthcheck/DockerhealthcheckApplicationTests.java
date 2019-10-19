8f75dcbfd415bedeafde27eea0d55602223e7e06
zq2599 <zq2599@gmail.com>
2019-10-20 01:10:08 +0800

    增减容器健康检查用的demo

---
+++ dockerhealthcheck/src/test/java/com/bolingcavalry/dockerhealthcheck/DockerhealthcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolingcavalry.dockerhealthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerhealthcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
