d2d3f20df8bb355b9ef3ef94b26149d131b82465
zq2599 <zq2599@gmail.com>
2020-05-25 08:48:08 +0800

    提交springboot-2.3.0的docker镜像demo

---
+++ dockerlayerdemo/src/main/java/com/bolingcavalry/dockerlayerdemo/DockerlayerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bolingcavalry.dockerlayerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerlayerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerlayerdemoApplication.class, args);
+	}
+
+}
