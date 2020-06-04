b191e0418ec924ca7f248c5e6a110ad5fbfdb2be
zq2599 <zq2599@gmail.com>
2020-06-04 18:14:29 +0800

    修复404问题，增加demo功能

--- probedemo/src/main/java/com/bolingcavalry/probedemo/ProbedemoApplication.java
+++ probedemo/src/main/java/com/bolingcavalry/probedemo/ProbedemoApplication.java
@@ -2,12 +2,31 @@ package com.bolingcavalry.probedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import javax.annotation.Resource;
+import java.util.Date;
+
+@SpringBootApplication(scanBasePackages = "com.bolingcavalry.probedemo")
 public class ProbedemoApplication {
+
+    @Resource
+    ApplicationAvailability applicationAvailability;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProbedemoApplication.class, args);
+    }
+
+    @RequestMapping(value="/get1")
+    public String state(){
+        return "livenessState : " + applicationAvailability.getLivenessState()
+                + "<br>readinessState : " + applicationAvailability.getReadinessState()
+                + "<br>" + new Date();
     }
 
 }
