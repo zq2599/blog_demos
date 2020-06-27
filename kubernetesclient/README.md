8b8a6cb0d50332488b6343dbaafeb4d6a57cc03c
zq2599 <zq2599@gmail.com>
2020-06-27 22:04:12 +0800

    将运行成功的代码提交

---
+++ kubernetesclient/README.md
@@ -0,0 +1,3 @@
+# kubernetesclient
+
+kubernetesclient demo
--- kubernetesclient/helloworld/src/main/java/com/bolingcavalry/demo/DemoApplication.java
+++ kubernetesclient/helloworld/src/main/java/com/bolingcavalry/demo/DemoApplication.java
@@ -5,14 +5,12 @@ import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.V1PodList;
-import io.kubernetes.client.util.ClientBuilder;
-import io.kubernetes.client.util.KubeConfig;
+import io.kubernetes.client.util.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -28,31 +26,15 @@ public class DemoApplication {
 
     @RequestMapping(value = "/hello")
     public String hello() throws Exception {
-
-        String kubeConfigPath = "D://temp//config";
-
-        // loading the out-of-cluster config, a kubeconfig from file-system
-        ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
-
-        // set the global default api-client to the in-cluster one from above
+        ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
 
-        // the CoreV1Api loads default api-client from global configuration.
         CoreV1Api api = new CoreV1Api();
-
-        V1PodList list =
-                api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
 
         List<String> rlt = new ArrayList<>();
         rlt.add(new Date().toString());
-        rlt.addAll(
-                list
-                  .getItems()
-                  .stream()
-                  .map(value -> value.getMetadata().getNamespace()
-                                + ":"
-                                + value.getMetadata().getName())
-                  .collect(Collectors.toList()));
+        rlt.addAll(list.getItems().stream().map(value -> value.getMetadata().getNamespace() + ":" + value.getMetadata().getName()).collect(Collectors.toList()));
         return new Gson().toJson(rlt);
     }
 
