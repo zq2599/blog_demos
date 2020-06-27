222b07166e1b7b91aa2f9b87b0517d08bc411912
zq2599 <zq2599@gmail.com>
2020-06-27 10:21:39 +0800

    增加k8s客户端的父子工程

---
+++ kubernetesclient/helloworld/src/main/java/com/bolingcavalry/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.bolingcavalry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello " + new Date();
+    }
+
+}
