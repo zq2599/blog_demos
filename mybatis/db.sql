e14845e56f1bbd7ea7900e0c0bffcc0ad58328b4
zq2599 <zq2599@gmail.com>
2020-08-07 08:40:15 +0800

    增加log表的映射

--- mybatis/db.sql
+++ mybatis/db.sql
@@ -1,8 +1,19 @@
-drop table user;
+DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `id` int(32) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `age` int(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `log`;
+
+CREATE TABLE `log` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `user_id` int(32),
+  `action` varchar(255) NOT NULL,
+  `create_time` datetime not null,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
--- mybatis/simple/src/main/java/com/bolingcavalry/simple/SimpleApplication.java
+++ mybatis/simple/src/main/java/com/bolingcavalry/simple/SimpleApplication.java
@@ -3,7 +3,6 @@ package com.bolingcavalry.simple;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.bolingcavalry.simple.mapper")
