dfd9789cc3f4daa04212472a89c39afd166394a7
zq2599 <zq2599@gmail.com>
2020-08-27 08:28:39 +0800

    jackson的demo增加集合操作

--- jacksondemo/databind/src/main/java/com/bolingcavalry/jacksondemo/databind/SimpleDemo.java
+++ jacksondemo/databind/src/main/java/com/bolingcavalry/jacksondemo/databind/SimpleDemo.java
@@ -9,8 +9,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @Description: (这里用一句话描述这个类的作用)
@@ -63,7 +63,7 @@ public class SimpleDemo {
         TwitterEntry tFromFile = mapper.readValue(new File("twitter.json"), TwitterEntry.class);
         logger.info("从文件反序列化的对象：{}", tFromStr);
 
-        // 文件 -> 对象
+        // byte数组 -> 对象
         TwitterEntry tFromBytes = mapper.readValue(array, TwitterEntry.class);
         logger.info("从byte数组反序列化的对象：{}", tFromBytes);
 
@@ -93,7 +93,7 @@ public class SimpleDemo {
         Map<String, Object> mapFromStr = mapper.readValue(mapJsonStr, new TypeReference<Map<String, Object>>() {});
         logger.info("从字符串反序列化的HashMap对象：{}", mapFromStr);
 
-        //
+        // JsonNode类型操作
         JsonNode jsonNode = mapper.readTree(mapJsonStr);
         String name = jsonNode.get("name").asText();
         int age = jsonNode.get("age").asInt();
@@ -102,5 +102,50 @@ public class SimpleDemo {
 
         logger.info("用JsonNode对象和API反序列化得到的数：name[{}]、age[{}]、city[{}]、street[{}]", name, age, city, street);
 
+        // 时间类型格式
+
+        Map<String, Object> dateMap = new HashMap<>();
+        dateMap.put("today", new Date());
+
+        String dateMapStr = mapper.writeValueAsString(dateMap);
+        logger.info("默认的时间序列化：{}", dateMapStr);
+
+        // 设置时间格式
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+        dateMapStr = mapper.writeValueAsString(dateMap);
+        logger.info("自定义的时间序列化：{}", dateMapStr);
+
+        System.out.println(objectJsonStr);
+
+        // json数组
+        String jsonArrayStr = "[{\n" +
+                "  \"id\":1,\n" +
+                "  \"text\":\"text1\",\n" +
+                "  \"fromUserId\":11, \n" +
+                "  \"toUserId\":111,\n" +
+                "  \"languageCode\":\"en\"\n" +
+                "},\n" +
+                "{\n" +
+                "  \"id\":2,\n" +
+                "  \"text\":\"text2\",\n" +
+                "  \"fromUserId\":22, \n" +
+                "  \"toUserId\":222,\n" +
+                "  \"languageCode\":\"zh\"\n" +
+                "},\n" +
+                "{\n" +
+                "  \"id\":3,\n" +
+                "  \"text\":\"text3\",\n" +
+                "  \"fromUserId\":33, \n" +
+                "  \"toUserId\":333,\n" +
+                "  \"languageCode\":\"en\"\n" +
+                "}]";
+
+        // json数组 -> 对象数组
+        TwitterEntry[] twitterEntryArray = mapper.readValue(jsonArrayStr, TwitterEntry[].class);
+        logger.info("json数组反序列化成对象数组：{}", Arrays.toString(twitterEntryArray));
+
+        // json数组 -> 对象集合
+        List<TwitterEntry> twitterEntryList = mapper.readValue(jsonArrayStr, new TypeReference<List<TwitterEntry>>() {});
+        logger.info("json数组反序列化成对象集合：{}", twitterEntryList);
     }
 }
