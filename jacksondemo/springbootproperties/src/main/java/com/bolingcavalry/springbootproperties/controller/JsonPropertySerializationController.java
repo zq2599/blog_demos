a266f6c0bac9060bc2065e81464318e3823a27cc
zq2599 <zq2599@gmail.com>
2020-09-10 08:32:10 +0800

    增加springboot集成jackson的demo

--- jacksondemo/springbootproperties/src/main/java/com/bolingcavalry/springbootproperties/controller/JsonPropertySerializationController.java
+++ jacksondemo/springbootproperties/src/main/java/com/bolingcavalry/springbootproperties/controller/JsonPropertySerializationController.java
@@ -1,19 +1,20 @@
 package com.bolingcavalry.springbootproperties.controller;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.bolingcavalry.springbootproperties.bean.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 /**
- * @Description: log相关的web接口
+ * @Description: 序列化和反序列化测试
  * @author: willzhao E-mail: zq2599@gmail.com
  * @date: 2020/8/4 8:31
  */
@@ -22,39 +23,24 @@ import java.util.Date;
 @Api(tags = {"JsonPropertySerializationController"})
 public class JsonPropertySerializationController {
 
-    @ApiModel(description = "JsonProperty注解测试类")
-    static class Test {
+    private static final Logger logger = LoggerFactory.getLogger(JsonPropertySerializationController.class);
 
-        @ApiModelProperty(value = "私有成员变量")
-        @JsonProperty(value = "json_field0", index = 1)
-        private Date field0 = new Date();
-
-        @ApiModelProperty(value = "来自get方法的字符串")
-        @JsonProperty(value = "json_field1", index = 0)
-        public String getField1() {
-            return "111";
-        }
-
-        @Override
-        public String toString() {
-            return "Test{" +
-                    "field0=" + field0 +
-                    '}';
-        }
-
-    }
+    @Autowired
+    ObjectMapper mapper;
 
     @ApiOperation(value = "测试序列化", notes = "测试序列化")
     @RequestMapping(value = "/serialization", method = RequestMethod.GET)
-    public Test jsonproperty() {
-        return new Test();
-    }
+    public Test jsonproperty() throws JsonProcessingException {
 
+        Test test = new Test();
+        logger.info(mapper.writeValueAsString(test));
+
+        return test;
+    }
 
     @ApiOperation(value = "测试反序列化", notes="测试反序列化")
     @RequestMapping(value = "/deserialization",method = RequestMethod.PUT)
     public String create(@RequestBody Test test) {
         return test.toString();
     }
-
 }
