c6d864e91b505fc765835325f904c95ff9257833
zq2599 <zq2599@gmail.com>
2020-09-30 08:30:59 +0800

    增加assert和assuem的测试

---
+++ junitpractice/assertassume/src/main/java/com/bolingcavalry/assertassume/AssertAssumeApplication.java
@@ -0,0 +1,17 @@
+package com.bolingcavalry.assertassume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 启动类
+ * @author: willzhao E-mail: zq2599@gmail.com
+ * @date: 2020/9/20 15:53
+ */
+@SpringBootApplication
+public class AssertAssumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssertAssumeApplication.class, args);
+    }
+}
