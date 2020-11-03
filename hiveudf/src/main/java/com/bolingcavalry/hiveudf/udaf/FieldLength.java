7a52a063f487a134c534058afef9a30aa0bd362f
zq2599 <zq2599@gmail.com>
2020-11-03 09:59:11 +0800

    增加hive的UDF实战项目

---
+++ hiveudf/src/main/java/com/bolingcavalry/hiveudf/udaf/FieldLength.java
@@ -0,0 +1,12 @@
+package com.bolingcavalry.hiveudf.udaf;
+
+import org.apache.hadoop.hive.ql.udf.generic.AbstractGenericUDAFResolver;
+
+/**
+ * @Description: (这里用一句话描述这个类的作用)
+ * @author: willzhao E-mail: zq2599@gmail.com
+ * @date: 2020/11/3 9:08
+ */
+public class FieldLength extends AbstractGenericUDAFResolver {
+
+}
