6af11f09ceb72e06287906416e79010afa882974
zq2599 <zq2599@gmail.com>
2020-11-28 11:45:33 +0800

    将大文件迁移到新仓库，在原目录增加说明

--- tomcat_openresty_docker_image_files/nginx_upstream_check_module-0.3.0/test/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-TEST_NGINX_USE_HUP=1 PATH=/home/yaoweibin/nginx/sbin:$PATH prove -r t
--- tomcat_openresty_docker_image_files/ngx_openresty-1.7.7.2/bundle/set-misc-nginx-module-0.28/src/ngx_http_set_hex.h
+++
@@ -1,9 +0,0 @@
-#include <ngx_core.h>
-#include <ngx_config.h>
-#include <ngx_http.h>
-
-ngx_int_t ngx_http_set_misc_set_decode_hex(ngx_http_request_t *r,
-        ngx_str_t *res, ngx_http_variable_value_t *v);
-
-ngx_int_t ngx_http_set_misc_set_encode_hex(ngx_http_request_t *r,
-        ngx_str_t *res, ngx_http_variable_value_t *v);
