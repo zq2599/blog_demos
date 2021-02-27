c2993b60aeb59bb928f505ce3f4464e7d57640b2
zq2599 <zq2599@gmail.com>
2021-02-28 00:42:09 +0800

    完善webhook的代码

--- kubebuilder/elasticweb/api/v1/elasticweb_webhook.go
+++ kubebuilder/elasticweb/api/v1/elasticweb_webhook.go
@@ -17,7 +17,10 @@ limitations under the License.
 package v1
 
 import (
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -43,6 +46,14 @@ func (r *ElasticWeb) Default() {
 	elasticweblog.Info("default", "name", r.Name)
 
 	// TODO(user): fill in your defaulting logic.
+	// 如果创建的时候没有输入总QPS，就设置个默认值
+	if r.Spec.TotalQPS == nil {
+		r.Spec.TotalQPS = new(int32)
+		*r.Spec.TotalQPS = 1300
+		elasticweblog.Info("a. TotalQPS is nil, set default value now", "TotalQPS", *r.Spec.TotalQPS)
+	} else {
+		elasticweblog.Info("b. TotalQPS exists", "TotalQPS", *r.Spec.TotalQPS)
+	}
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -55,7 +66,8 @@ func (r *ElasticWeb) ValidateCreate() error {
 	elasticweblog.Info("validate create", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+
+	return r.validateElasticWeb()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
@@ -63,7 +75,7 @@ func (r *ElasticWeb) ValidateUpdate(old runtime.Object) error {
 	elasticweblog.Info("validate update", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return r.validateElasticWeb()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -72,4 +84,26 @@ func (r *ElasticWeb) ValidateDelete() error {
 
 	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
+}
+
+func (r *ElasticWeb) validateElasticWeb() error {
+	var allErrs field.ErrorList
+
+	if *r.Spec.SinglePodQPS > 1000 {
+		elasticweblog.Info("c. Invalid SinglePodQPS")
+
+		err := field.Invalid(field.NewPath("spec").Child("singlePodQPS"),
+			*r.Spec.SinglePodQPS,
+			"d. must be less than 1000")
+
+		allErrs = append(allErrs, err)
+
+		return apierrors.NewInvalid(
+			schema.GroupKind{Group: "elasticweb.com.bolingcavalry", Kind: "ElasticWeb"},
+			r.Name,
+			allErrs)
+	} else {
+		elasticweblog.Info("e. SinglePodQPS is valid")
+		return nil
+	}
 }
