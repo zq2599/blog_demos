8f22cfb319a4b37aa2796665fab43342bcd9675a
zq2599 <zq2599@gmail.com>
2021-03-27 11:32:22 +0800

    修改gradle脚本的bug

--- gradlespringbootdemo/democlient/src/main/java/com/bolingcavalry/dto/Student.java
+++ gradlespringbootdemo/democlient/src/main/java/com/bolingcavalry/dto/Student.java
@@ -1,13 +1,19 @@
 package com.bolingcavalry.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString
-@Builder
 public class Student {
     String name;
     int age;
+
+    public static void test(){
+        new Student("1",2);
+    }
 }
--- gradlespringbootdemo/webapp/src/main/java/com/bolingcavalry/DemoApplication.java
+++ gradlespringbootdemo/webapp/src/main/java/com/bolingcavalry/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.bolingcavalry;
 
+import com.bolingcavalry.dto.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,23 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-/**
- * @Description: (这里用一句话描述这个类的作用)
- * @author: willzhao E-mail: zq2599@gmail.com
- * @date: 2021/3/21 8:12
- */
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello " + new Date();
+        return "Hello "
+                + new Date()
+                + "--"
+                + new Student("Tom", 11);
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
