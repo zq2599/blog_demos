3a92731622352e217d4ac7cdc58474de3fa0724f
zq2599 <zq2599@gmail.com>
2021-04-02 08:53:49 +0800

    增加etcd基本操作的demo

---
+++ jetcd-tutorials/base-operate/src/main/java/com/bolingcavalry/dao/impl/EtcdServiceImpl.java
@@ -0,0 +1,71 @@
+package com.bolingcavalry.dao.impl;
+
+import com.bolingcavalry.dao.EtcdService;
+import io.etcd.jetcd.ByteSequence;
+import io.etcd.jetcd.Client;
+import io.etcd.jetcd.KV;
+import io.etcd.jetcd.Response;
+import io.etcd.jetcd.kv.GetResponse;
+import io.etcd.jetcd.kv.PutResponse;
+import io.etcd.jetcd.options.DeleteOption;
+import io.etcd.jetcd.options.GetOption;
+
+import static com.google.common.base.Charsets.UTF_8;
+
+/**
+ * @Description: etcd服务的实现类
+ * @author: willzhao E-mail: zq2599@gmail.com
+ * @date: 2021/3/30 8:28
+ */
+public class EtcdServiceImpl implements EtcdService {
+
+    private static final String IP = "192.168.133.218";
+
+    /**
+     * 将字符串转为客户端所需的ByteSequence实例
+     * @param val
+     * @return
+     */
+    private static ByteSequence bytesOf(String val) {
+        return ByteSequence.from(val, UTF_8);
+    }
+
+    /**
+     * 新建key-value客户端实例
+     * @return
+     */
+    private KV getKVClient(){
+        String endpoints = "http://" + IP + ":2379,http://" + IP + ":2380,http://" + IP + ":2381";
+        Client client = Client.builder().endpoints(endpoints.split(",")).build();
+        return client.getKVClient();
+    }
+
+    @Override
+    public Response.Header put(String key, String value) throws Exception {
+        return getKVClient().put(bytesOf(key), bytesOf(value)).get().getHeader();
+    }
+
+    @Override
+    public String getSingle(String key) throws Exception {
+        GetResponse getResponse = getKVClient().get(bytesOf(key)).get();
+
+        return getResponse.getCount()>0 ?
+               getResponse.getKvs().get(0).getValue().toString(UTF_8) :
+               null;
+    }
+
+    @Override
+    public GetResponse getRange(String key, GetOption getOption) throws Exception {
+        return getKVClient().get(bytesOf(key), getOption).get();
+    }
+
+    @Override
+    public long deleteSingle(String key) throws Exception {
+        return getKVClient().delete(bytesOf(key)).get().getDeleted();
+    }
+
+    @Override
+    public long deleteRange(String key, DeleteOption deleteOption) throws Exception {
+        return getKVClient().delete(bytesOf(key), deleteOption).get().getDeleted();
+    }
+}
