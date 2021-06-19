61e4ee2685590f77dcd04ca32883d92a950d7a04
zq2599 <zq2599@gmail.com>
2021-06-19 22:24:36 +0800

    增加注释

--- kurentodemo/player-with-record/src/main/java/com/bolingcavalry/playerwithrecord/PlayerWithRecorder.java
+++ kurentodemo/player-with-record/src/main/java/com/bolingcavalry/playerwithrecord/PlayerWithRecorder.java
@@ -24,6 +24,10 @@ public class PlayerWithRecorder implements WebSocketConfigurer {
     return new PlayerHandler();
   }
 
+  /**
+   * 实例化KurentoClient，入参是KMS地址
+   * @return
+   */
   @Bean
   public KurentoClient kurentoClient() {
     return KurentoClient.create("ws://192.168.91.128:8888/kurento");
@@ -36,6 +40,10 @@ public class PlayerWithRecorder implements WebSocketConfigurer {
     return container;
   }
 
+  /**
+   * 标准的WebSocket处理类绑定
+   * @param registry
+   */
   @Override
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
     registry.addHandler(handler(), "/player");
