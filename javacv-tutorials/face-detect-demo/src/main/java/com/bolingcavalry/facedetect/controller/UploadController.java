e6b6a609eebf9dbf5b049d8564d9fbe3a28a2f43
zq2599 <zq2599@gmail.com>
2021-07-16 08:41:38 +0800

    增加javacv的demo

---
+++ javacv-tutorials/face-detect-demo/src/main/java/com/bolingcavalry/facedetect/controller/UploadController.java
@@ -0,0 +1,80 @@
+package com.bolingcavalry.facedetect.controller;
+
+import com.bolingcavalry.facedetect.util.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Controller
+public class UploadController {
+
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public UploadController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @Value("${web.upload-path}")
+    private String path;
+
+    /**
+     * 跳转到文件上传页面
+     * @return
+     */
+    @RequestMapping("test")
+    public String toUpload(){
+        return "test";
+    }
+
+    /**
+     *
+     * @param file 要上传的文件
+     * @return
+     */
+    @RequestMapping("fileUpload")
+    public String upload(@RequestParam("fileName") MultipartFile file, Map<String, Object> map){
+
+        // 要上传的目标文件存放路径
+        String localPath = "E:/Develop/Files/Photos";
+        // 上传成功或者失败的提示
+        String msg = "";
+
+        if (FileUtils.upload(file, path, file.getOriginalFilename())){
+            // 上传成功，给出页面提示
+            msg = "上传成功！";
+        }else {
+            msg = "上传失败！";
+
+        }
+
+        // 显示图片
+        map.put("msg", msg);
+        map.put("fileName", file.getOriginalFilename());
+
+        return "forward:/test";
+    }
+
+    /**
+     * 显示单张图片
+     * @return
+     */
+    @RequestMapping("show")
+    public ResponseEntity showPhotos(String fileName){
+
+        try {
+            // 由于是读取本机的文件，file是一定要加上的， path是在application配置文件中的路径
+            return ResponseEntity.ok(resourceLoader.getResource("file:" + path + fileName));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
