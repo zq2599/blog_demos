f4685bac1173e1e0f44ff1829456a564d00e167c
zq2599 <zq2599@gmail.com>
2021-07-25 12:04:07 +0800

    调整代码

--- javacv-tutorials/ffmpeg-basic/src/main/java/com/bolingcavalry/basic/Stream2Image.java
+++ javacv-tutorials/ffmpeg-basic/src/main/java/com/bolingcavalry/basic/Stream2Image.java
@@ -1,15 +1,18 @@
 package com.bolingcavalry.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.javacpp.*;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import static org.bytedeco.javacpp.avcodec.*;
 import static org.bytedeco.javacpp.avformat.*;
 import static org.bytedeco.javacpp.avutil.*;
 import static org.bytedeco.javacpp.swscale.*;
+import static org.bytedeco.javacpp.swscale.sws_freeContext;
 
 /**
  * @author willzhao
@@ -17,90 +20,193 @@ import static org.bytedeco.javacpp.swscale.*;
  * @description 体验FFmpeg的常用API和数据结构
  * @date 2021/7/24 15:40
  */
+@Slf4j
 public class Stream2Image {
 
-    public int openVideo(String url,String out_file) throws IOException {
-        // 指针
-        BytePointer buffer = null;
+    public void openVideo(String url,String out_file) throws IOException {
+        log.error("正在打开流媒体 [{}]", url);
 
-        // 解码前的数据
-        AVPacket packet = new AVPacket();
-        // 解码后的数据
-        AVFrame pFrame = null;
-        // 解码后的数据
-        AVFrame pFrameRGB = null;
+        // 打开指定流媒体，进行解封装，得到解封装上下文
+        AVFormatContext pFormatCtx = getFormatContext(url);
 
-        //解码器
-        AVCodec pCodec = null;
+        if (null==pFormatCtx) {
+            log.error("获取解封装上下文失败");
+            return;
+        }
 
-        // 解码上下文
-        AVCodecContext pCodecCtx = null;
+        // 控制台打印流媒体信息
+        av_dump_format(pFormatCtx, 0, url, 0);
+
+        // 流媒体解封装后有一个保存了所有流的数组，videoStreamIndex表示视频流在数组中的位置
+        int videoStreamIndex = getVideoStreamIndex(pFormatCtx);
+
+        // 找不到视频流就直接返回
+        if (videoStreamIndex<0) {
+            log.error("没有找到视频流");
+            return;
+        }
+
+        log.info("视频流在流数组中的第[{}]个流是视频流(从0开始)", videoStreamIndex);
+
+        // 得到解码上下文，已经完成了初始化
+        AVCodecContext pCodecCtx = getCodecContext(pFormatCtx, videoStreamIndex);
+
+        if (null==pCodecCtx) {
+            log.error("生成解码上下文失败");
+            return;
+        }
+
+        // 从视频流中解码一帧
+        AVFrame pFrame = getSingleFrame(pCodecCtx,pFormatCtx, videoStreamIndex);
+
+        if (null==pFrame) {
+            log.error("从视频流中取帧失败");
+            return;
+        }
+
+        // 将YUV420P图像转成YUVJ420P
+        // 转换后的图片的AVFrame，及其对应的数据指针，都放在frameData对象中
+        FrameData frameData = YUV420PToYUVJ420P(pCodecCtx, pFrame);
+
+        if (null==frameData) {
+            log.info("YUV420P格式转成YUVJ420P格式失败");
+            return;
+        }
+
+        // 持久化存储
+        saveImg(frameData.avFrame,out_file);
+
+        // 按顺序释放
+//        release(null, pFormatCtx.pb(), pCodecCtx, pFormatCtx, frameData.buffer, frameData.avFrame, pFrame);
+        release(true, null, null, pCodecCtx, pFormatCtx, frameData.buffer, frameData.avFrame, pFrame);
+
+        log.info("操作成功");
+    }
+
+    /**
+     * 生成解封装上下文
+     * @param url
+     * @return
+     */
+    private AVFormatContext getFormatContext(String url) {
         // 解封装上下文
         AVFormatContext pFormatCtx = new avformat.AVFormatContext(null);
 
-        int[] frameFinished = new int[1];
-
-        avutil.AVDictionary optionsDict = null;
-
-        int i, videoStream;
-
-        int numBytes;
-
-        // 图片处理的上下文
-        swscale.SwsContext sws_ctx = null;
-
         // 打开流媒体
         if (avformat_open_input(pFormatCtx, url, null, null) != 0) {
-            return -1; // Couldn't open file
+            return null;
         }
 
         // 读取流媒体数据，以获得流的信息
         if (avformat_find_stream_info(pFormatCtx, (PointerPointer<Pointer>) null) < 0) {
-            return -1;// Couldn't find stream information
+            return null;
         }
 
-        // 控制台打印流媒体信息
-        av_dump_format(pFormatCtx, 0, url, 0);// Dump information about file onto standard error
+        return pFormatCtx;
+    }
 
-        videoStream = -1;
+    /**
+     * 生成解码上下文
+     * @param pFormatCtx
+     * @param videoStreamIndex
+     * @return
+     */
+    private AVCodecContext getCodecContext(AVFormatContext pFormatCtx, int videoStreamIndex) {
+        //解码器
+        AVCodec pCodec;
+
+        // 得到解码上下文
+        AVCodecContext pCodecCtx = pFormatCtx.streams(videoStreamIndex).codec();
+
+        // 根据解码上下文得到解码器
+        pCodec = avcodec_find_decoder(pCodecCtx.codec_id());
+
+        if (pCodec == null) {
+            return null;
+        }
+
+        // 用解码器来初始化解码上下文
+        if (avcodec_open2(pCodecCtx, pCodec, (AVDictionary)null) < 0) {
+            return null;
+        }
+
+        return pCodecCtx;
+    }
+
+    /**
+     * 取一帧然后解码
+     * @param pCodecCtx
+     * @param pFormatCtx
+     * @param videoStreamIndex
+     * @return
+     */
+    private AVFrame getSingleFrame(AVCodecContext pCodecCtx, AVFormatContext pFormatCtx, int videoStreamIndex) {
+        // 分配帧对象
+        AVFrame pFrame = av_frame_alloc();
+
+        // frameFinished用于检查是否有图像
+        int[] frameFinished = new int[1];
+
+        // 是否找到的标志
+        boolean exists = false;
+
+        AVPacket packet = new AVPacket();
+
+        try {
+            // 每一次while循环都会读取一个packet
+            while (av_read_frame(pFormatCtx, packet) >= 0) {
+                // 检查packet所属的流是不是视频流
+                if (packet.stream_index() == videoStreamIndex) {
+                    // 将AVPacket解码成AVFrame
+                    avcodec_decode_video2(pCodecCtx, pFrame, frameFinished, packet);// Decode video frame
+
+                    // 如果有图像就返回
+                    if (frameFinished != null && frameFinished[0] != 0 && !pFrame.isNull()) {
+                        exists = true;
+                        break;
+                    }
+                }
+            }
+        } finally {
+            // 一定要执行释放操作
+            av_free_packet(packet);
+        }
+
+        // 找不到就返回空
+        return exists ?  pFrame : null;
+    }
+
+    /**
+     * 流媒体解封装后得到多个流组成的数组，该方法找到视频流咋数组中的位置
+     * @param pFormatCtx
+     * @return
+     */
+    private static int getVideoStreamIndex(AVFormatContext pFormatCtx) {
+        int videoStream = -1;
 
         // 解封装后有多个流，找出视频流是第几个
-        for (i = 0; i < pFormatCtx.nb_streams(); i++) {
+        for (int i = 0; i < pFormatCtx.nb_streams(); i++) {
             if (pFormatCtx.streams(i).codec().codec_type() == AVMEDIA_TYPE_VIDEO) {
                 videoStream = i;
                 break;
             }
         }
 
-        // 找不到就返回了
-        if (videoStream == -1) {
-            return -1; // Didn't find a video stream
-        }
+        return videoStream;
+    }
 
-        // 得到解码上下文
-        pCodecCtx = pFormatCtx.streams(videoStream).codec();
-
-        // 根据解码上下文得到解码器
-        pCodec = avcodec_find_decoder(pCodecCtx.codec_id());
-
-        if (pCodec == null) {
-            System.err.println("Unsupported codec!");
-            return -1; // Codec not found
-        }
-
-        // 用解码器来初始化解码上下文
-        if (avcodec_open2(pCodecCtx, pCodec, optionsDict) < 0) {
-            return -1; // Could not open codec
-        }
-
-        // 分配一个帧对象，保存解码后的一帧YUV420P图像
-        pFrame = av_frame_alloc();
-
+    /**
+     * 将YUV420P格式的图像转为YUVJ420P格式
+     * @param pCodecCtx 解码上下文
+     * @param sourceFrame 源数据
+     * @return 转换后的帧极其对应的数据指针
+     */
+    private static FrameData YUV420PToYUVJ420P(AVCodecContext pCodecCtx, AVFrame sourceFrame) {
         // 分配一个帧对象，保存从YUV420P转为YUVJ420P的结果
-        pFrameRGB = av_frame_alloc();
+        AVFrame pFrameRGB = av_frame_alloc();
 
         if (pFrameRGB == null) {
-            return -1;
+            return null;
         }
 
         int width = pCodecCtx.width(), height = pCodecCtx.height();
@@ -110,92 +216,78 @@ public class Stream2Image {
         pFrameRGB.height(height);
         pFrameRGB.format(AV_PIX_FMT_YUVJ420P);
 
-        // Determine required buffer size and allocate buffer
         // 计算转为YUVJ420P之后的图片字节数
-        numBytes = avpicture_get_size(AV_PIX_FMT_YUVJ420P, width, height);
+        int numBytes = avpicture_get_size(AV_PIX_FMT_YUVJ420P, width, height);
 
         // 分配内存
-        buffer = new BytePointer(av_malloc(numBytes));
+        BytePointer buffer = new BytePointer(av_malloc(numBytes));
 
         // 图片处理工具的初始化操作
-        sws_ctx = sws_getContext(pCodecCtx.width(), pCodecCtx.height(), pCodecCtx.pix_fmt(), pCodecCtx.width(),
-                pCodecCtx.height(), AV_PIX_FMT_YUVJ420P, SWS_BICUBIC, null, null, (DoublePointer) null);
+        SwsContext sws_ctx = sws_getContext(width, height, pCodecCtx.pix_fmt(), width, height, AV_PIX_FMT_YUVJ420P, SWS_BICUBIC, null, null, (DoublePointer) null);
 
         // 将pFrameRGB的data指针指向刚才分配好的内存(即buffer)
-        avpicture_fill(new avcodec.AVPicture(pFrameRGB), buffer, AV_PIX_FMT_YUVJ420P, pCodecCtx.width(), pCodecCtx.height());
+        avpicture_fill(new avcodec.AVPicture(pFrameRGB), buffer, AV_PIX_FMT_YUVJ420P, width, height);
 
-        int ret=-1;
+        // 转换图像格式，将解压出来的YUV420P的图像转换为YUVJ420P的图像
+        sws_scale(sws_ctx, sourceFrame.data(), sourceFrame.linesize(), 0, height, pFrameRGB.data(), pFrameRGB.linesize());
 
-        // 每一次while循环都会读取一个packet
-        while (av_read_frame(pFormatCtx, packet) >= 0) {
-            // 检查packet所属的流是不是视频流
-            if (packet.stream_index() == videoStream) {
-                // 将解封装后的AVPacket进行解码，解码后就是AVFrame
-                avcodec_decode_video2(pCodecCtx, pFrame, frameFinished, packet);// Decode video frame
+        // 及时释放
+        sws_freeContext(sws_ctx);
 
-                // Did we get a video frame?
-//                if (frameFinished != null) {
-//                    sws_scale(sws_ctx, pFrame.data(), pFrame.linesize(), 0, pCodecCtx.height(), pFrameRGB.data(),
-//                            pFrameRGB.linesize());
-//                }
-                // 这里怎么和前面重复了？
-                if (frameFinished != null && frameFinished[0] != 0 && !pFrame.isNull()) {
-                    // Convert the image from its native format to YUVJ420P
-                    // 转换图像格式，将解压出来的YUV420P的图像转换为YUVJ420P的图像
-                    sws_scale(sws_ctx, pFrame.data(), pFrame.linesize(), 0, pCodecCtx.height(), pFrameRGB.data(),pFrameRGB.linesize());
-                    if((ret=saveImg(pFrameRGB,out_file))>=0) {
-                        break;
-                    }
-                }
-            }
-
-        }
-        av_free_packet(packet);// Free the packet that was allocated by av_read_frame
-        // Free the RGB image
-        av_free(buffer);
-
-        av_free(pFrameRGB);
-
-        av_free(pFrame);// Free the YUV frame
-
-        avcodec_close(pCodecCtx);// Close the codec
-
-        avformat_close_input(pFormatCtx);// Close the video file
-
-        return ret;
+        // 将AVFrame和BytePointer打包到FrameData中返回，这两个对象都要做显示的释放操作
+        return new FrameData(pFrameRGB, buffer);
     }
 
 
+    /**
+     * 将传入的帧以图片的形式保存在指定位置
+     * @param pFrame
+     * @param out_file
+     * @return true 成功 false 失败
+     */
     private int saveImg(avutil.AVFrame pFrame, String out_file) {
         av_log_set_level(AV_LOG_ERROR);//设置FFmpeg日志级别（默认是debug，设置成error可以屏蔽大多数不必要的控制台消息）
-        avcodec.AVPacket pkt = null;
-        avformat.AVStream pAVStream = null;
-        avcodec.AVCodec codec = null;
-        int ret = -1;
+
+        AVPacket pkt = null;
+        AVStream pAVStream = null;
 
         int width = pFrame.width(), height = pFrame.height();
+
         // 分配AVFormatContext对象
         avformat.AVFormatContext pFormatCtx = avformat_alloc_context();
-        // 设置输出文件格式
+
+        // 设置输出格式(涉及到封装和容器)
         pFormatCtx.oformat(av_guess_format("mjpeg", null, null));
+
         if (pFormatCtx.oformat() == null) {
+            log.error("输出媒体流的封装格式设置失败");
             return -1;
         }
+
         try {
             // 创建并初始化一个和该url相关的AVIOContext
             avformat.AVIOContext pb = new avformat.AVIOContext();
-            if (avio_open(pb, out_file, AVIO_FLAG_READ_WRITE) < 0) {// dont open file
+
+            // 打开输出文件
+            if (avio_open(pb, out_file, AVIO_FLAG_READ_WRITE) < 0) {
+                log.info("输出文件打开失败");
                 return -1;
             }
+
+            // 封装之上是协议，这里将封装上下文和协议上下文关联
             pFormatCtx.pb(pb);
+
             // 构建一个新stream
-            pAVStream = avformat_new_stream(pFormatCtx, codec);
+            pAVStream = avformat_new_stream(pFormatCtx, null);
+
             if (pAVStream == null) {
+                log.error("将新的流放入媒体文件失败");
                 return -1;
             }
+
             int codec_id = pFormatCtx.oformat().video_codec();
+
             // 设置该stream的信息
-            // AVCodecContext pCodecCtx = pAVStream.codec();
             avcodec.AVCodecContext pCodecCtx = pAVStream.codec();
             pCodecCtx.codec_id(codec_id);
             pCodecCtx.codec_type(AVMEDIA_TYPE_VIDEO);
@@ -205,63 +297,118 @@ public class Stream2Image {
             pCodecCtx.time_base().num(1);
             pCodecCtx.time_base().den(25);
 
-            // Begin Output some information
+            // 打印媒体信息
             av_dump_format(pFormatCtx, 0, out_file, 1);
-            // End Output some information
 
             // 查找解码器
             avcodec.AVCodec pCodec = avcodec_find_encoder(codec_id);
-            if (pCodec == null) {// codec not found
+            if (pCodec == null) {
+                log.info("获取解码器失败");
                 return -1;
             }
-            // 设置pCodecCtx的解码器为pCodec
+
+            // 用解码器来初始化解码上下文
             if (avcodec_open2(pCodecCtx, pCodec, (PointerPointer<Pointer>) null) < 0) {
-                System.err.println("Could not open codec.");
+                log.error("解码上下文初始化失败");
                 return -1;
             }
 
-            // Write Header
-            avformat_write_header(pFormatCtx, (PointerPointer<Pointer>) null);
-
-            // 给AVPacket分配足够大的空间
+            // 输出的Packet
             pkt = new avcodec.AVPacket();
+
+            // 分配
             if (av_new_packet(pkt, width * height * 3) < 0) {
                 return -1;
             }
+
             int[] got_picture = { 0 };
-            // encode
-            if (avcodec_encode_video2(pCodecCtx, pkt, pFrame, got_picture) >= 0) {
-                // flush
-                if ((ret = av_write_frame(pFormatCtx, pkt)) >= 0) {
-                    // Write Trailer
-                    if (av_write_trailer(pFormatCtx) >= 0) {
-                        System.err.println("Encode Successful.");
-                    }
-                }
+
+            // 把流的头信息写到要输出的媒体文件中
+            avformat_write_header(pFormatCtx, (PointerPointer<Pointer>) null);
+
+            // 把帧的内容进行编码
+            if (avcodec_encode_video2(pCodecCtx, pkt, pFrame, got_picture)<0) {
+                log.error("把帧编码为packet失败");
+                return -1;
             }
-            return ret;
-            // 结束时销毁
+
+            // 输出一帧
+            if ((av_write_frame(pFormatCtx, pkt)) < 0) {
+                log.error("输出一帧失败");
+                return -1;
+            }
+
+            // 写文件尾
+            if (av_write_trailer(pFormatCtx) < 0) {
+                log.error("写文件尾失败");
+                return -1;
+            }
+
+            return 0;
         } finally {
-            if (pkt != null) {
-                av_free_packet(pkt);
-            }
-            if (pAVStream != null) {
-                avcodec_close(pAVStream.codec());
-            }
-            if (pFormatCtx != null) {
-                avio_close(pFormatCtx.pb());
+            // 资源清理
+            release(false, pkt, pFormatCtx.pb(), pAVStream.codec(), pFormatCtx);
+        }
+    }
+
+    /**
+     * 释放资源，顺序是先释放数据，再释放上下文
+     * @param pCodecCtx
+     * @param pFormatCtx
+     * @param ptrs
+     */
+    private void release(boolean isInput, AVPacket pkt, AVIOContext pb, AVCodecContext pCodecCtx, AVFormatContext pFormatCtx, Pointer...ptrs) {
+
+
+        if (null!=pkt) {
+            av_free_packet(pkt);
+        }
+
+        // 解码后，这是个数组，要遍历处理
+        if (null!=ptrs) {
+            Arrays.stream(ptrs).forEach(avutil::av_free);
+        }
+
+        // 解码
+        if (null!=pCodecCtx) {
+            avcodec_close(pCodecCtx);
+        }
+
+        // 解协议
+        if (null!=pb) {
+            avio_close(pb);
+        }
+
+        // 解封装
+        if (null!=pFormatCtx) {
+            if (isInput) {
+                avformat_close_input(pFormatCtx);
+            } else {
                 avformat_free_context(pFormatCtx);
             }
         }
+
+
     }
 
     public static void main(String[] args) throws Exception {
 
-//        String url = "rtmp://58.200.131.2:1935/livetv/gdtv";
-        String url = "rtmp://58.200.131.2:1935/livetv/ahtv";
-//        String url = "E:\\temp\\202107\\24\\test.mp4";
+//        String url = "http://ivi.bupt.edu.cn/hls/cctv13hd.m3u8";
+//        String url = "rtmp://58.200.131.2:1935/livetv/ahtv";
+        String url = "E:\\temp\\202107\\24\\test.mp4";
         String localPath = "E:\\temp\\202107\\24\\save\\" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + ".jpg";
 
         new Stream2Image().openVideo(url, localPath);
+    }
+}
+
+class FrameData {
+
+    AVFrame avFrame;
+    BytePointer buffer;
+
+    public FrameData(AVFrame avFrame, BytePointer buffer) {
+        this.avFrame = avFrame;
+        this.buffer = buffer;
     }
 }
