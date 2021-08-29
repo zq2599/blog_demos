a29f6308658c8d7e0f97115dcdb2b3740a68369f
zq2599 <zq2599@gmail.com>
2021-08-29 23:54:42 +0800

    使限流demo运行起来

---
+++ spring-cloud-tutorials/gateway-requestratelimiter/src/main/java/com/bolingcavalry/gateway/config/CustomizeConfig.java
@@ -0,0 +1,21 @@
+package com.bolingcavalry.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author willzhao
+ * @version 1.0
+ * @description TODO
+ * @date 2021/8/22 23:41
+ */
+@Configuration
+public class CustomizeConfig {
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1");
+    }
+}
