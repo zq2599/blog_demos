97a7e72e7b603e8b0c059b45832bf6d3ef5585ef
zq2599 <zq2599@gmail.com>
2021-11-15 00:17:17 +0800

    可以成功读取视频帧的图片并存入本地

--- javacv-tutorials/simple-grab-push/src/main/java/com/bolingcavalry/grabpush/controller/GrabPushController.java
+++ javacv-tutorials/simple-grab-push/src/main/java/com/bolingcavalry/grabpush/controller/GrabPushController.java
@@ -2,8 +2,12 @@ package com.bolingcavalry.grabpush.controller;
 
 //import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RestController;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,10 +29,32 @@ public class GrabPushController {
 //        return "Success,  " + LocalDateTime.now();
 //    }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File tempFile = new File("/Users/zhaoqin/temp/202111/14/", "100410A000.mp4");
-        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(new FileInputStream(tempFile));
-        System.out.println(grabber);
+    public static void main(String[] args) throws Exception {
+//        File tempFile = new File("/Users/zhaoqin/temp/202111/14/", "100410A000.mp4");
+//        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(new FileInputStream(tempFile));
+
+        String url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(url);
+        grabber.start();
+        Frame frame;
+
+        Java2DFrameConverter java2DFrameConverter = new Java2DFrameConverter();
+
+        String imageMat = "jpg";
+
+        for (int i = 0 ; i < 10 ; i++) {
+            frame = grabber.grabImage();
+
+            BufferedImage bufferedImage = java2DFrameConverter.getBufferedImage(frame);
+
+            String fileName = "/Users/zhaoqin/temp/202111/15/images/video-frame-" + i + "." + imageMat;
+            File output = new File(fileName);
+
+            ImageIO.write(bufferedImage, imageMat, output);
+            System.out.println("save [" + fileName + "]");
+        }
+
+        grabber.stop();
     }
 
 
