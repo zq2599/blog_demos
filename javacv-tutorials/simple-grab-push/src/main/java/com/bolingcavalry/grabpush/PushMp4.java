825339431cd834307a34a411a1ff9e4d04b974f6
zq2599 <zq2599@gmail.com>
2021-11-20 18:03:04 +0800

    增加日志

--- javacv-tutorials/simple-grab-push/src/main/java/com/bolingcavalry/grabpush/PushMp4.java
+++ javacv-tutorials/simple-grab-push/src/main/java/com/bolingcavalry/grabpush/PushMp4.java
@@ -1,13 +1,15 @@
 package com.bolingcavalry.grabpush;
 
+import lombok.extern.slf4j.Slf4j;
+import org.bytedeco.ffmpeg.avcodec.AVCodecParameters;
+import org.bytedeco.ffmpeg.avformat.AVFormatContext;
+import org.bytedeco.ffmpeg.avformat.AVStream;
 import org.bytedeco.ffmpeg.global.avcodec;
+import org.bytedeco.ffmpeg.global.avutil;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
 import org.bytedeco.javacv.FFmpegLogCallback;
 import org.bytedeco.javacv.Frame;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 /**
  * @author willzhao
@@ -15,36 +17,153 @@ import java.io.InputStream;
  * @description TODO
  * @date 2021/11/19 8:49
  */
+@Slf4j
 public class PushMp4 {
-    public static void main(String[] args) throws Exception {
-        String pushUrl = "rtmp://192.168.50.43:11935/live/livestream2";
-        InputStream in = new FileInputStream("E:\\temp\\202107\\24\\test.mp4");
-        // 不能用maximumSize=0，否则会立即退出
-        //FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(in, 0);
-        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(in);
-        grabber.start();
-        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(pushUrl,
-                grabber.getImageWidth()*2,
-                grabber.getImageHeight()*2,
-                grabber.getAudioChannels());
+    /**
+     * 本地MP4文件的完整路径(两分零五秒的视频)
+     */
+//    private static final String MP4_FILE_PATH = "E:\\temp\\202111\\20\\sample-mp4-file.mp4";
+    private static final String MP4_FILE_PATH = "E:\\temp\\202111\\20\\Jellyfish_1080_10s_30MB.mp4";
 
-        int v_rs = 25;
+    /**
+     * SRS的推流地址
+     */
+    private static final String SRS_PUSH_ADDRESS = "rtmp://192.168.50.43:11935/live/livestream";
 
-        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264); // avcodec.AV_CODEC_ID_H264
-        recorder.setFormat("flv");
-        recorder.setFrameRate(v_rs);
-        recorder.setGopSize(v_rs);
-        recorder.setAudioChannels(grabber.getAudioChannels());
+
+    private static void grabAndPush(String sourceFilePath, String PUSH_ADDRESS) throws Exception {
+        // ffmepg日志级别
+        avutil.av_log_set_level(avutil.AV_LOG_ERROR);
         FFmpegLogCallback.set();
 
+        // 实例化帧抓取器对象，将文件路径传入
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(MP4_FILE_PATH);
+
+        long startTime = System.currentTimeMillis();
+
+        log.info("开始初始化帧抓取器");
+
+        // 初始化帧抓取器，例如数据结构（时间戳、编码器上下文、帧对象等），
+        // 如果入参等于true，还会调用avformat_find_stream_info方法获取流的信息，放入AVFormatContext类型的成员变量oc中
+        grabber.start(true);
+
+        log.info("帧抓取器初始化完成，耗时[{}]毫秒", System.currentTimeMillis()-startTime);
+
+        AVFormatContext avFormatContext = grabber.getFormatContext();
+
+        // 文件内有几个媒体流（一般是视频流+音频流）
+        int streamNum = avFormatContext.nb_streams();
+
+        // 没有媒体流就不用继续了
+        if (streamNum<1) {
+            log.error("文件内不存在媒体流");
+            return;
+        }
+
+        // 取得视频的帧率
+        int frameRate = (int)grabber.getVideoFrameRate();
+
+        log.info("视频帧率[{}]，视频时长[{}]秒，媒体流数量[{}]",
+                frameRate,
+                avFormatContext.duration()/1000000,
+                avFormatContext.nb_streams());
+
+        // 遍历每一个流，检查其类型
+        for (int i=0; i< streamNum; i++) {
+            AVStream avStream = avFormatContext.streams(i);
+            AVCodecParameters avCodecParameters = avStream.codecpar();
+            log.info("流的索引[{}]，编码器类型[{}]，编码器ID[{}]", i, avCodecParameters.codec_type(), avCodecParameters.codec_id());
+        }
+
+        int frameWidth = grabber.getImageWidth();
+        int frameHeight = grabber.getImageHeight();
+        int audioChannels = grabber.getAudioChannels();
+
+        log.info("视频宽度[{}]，视频高度[{}]，音频通道数[{}]",
+                frameWidth,
+                frameHeight,
+                audioChannels);
+
+        // 实例化FFmpegFrameRecorder，将SRS的推送地址传入
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(SRS_PUSH_ADDRESS,
+                frameWidth,
+                frameHeight,
+                audioChannels);
+
+        // 设置编码格式
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+
+        // 设置封装格式
+        recorder.setFormat("flv");
+
+        // 一秒内的帧数
+        recorder.setFrameRate(frameRate);
+
+        // 两个关键帧之间的帧数
+        recorder.setGopSize(frameRate);
+
+        // 设置音频通道数，与视频源的通道数相等
+        recorder.setAudioChannels(grabber.getAudioChannels());
+
+        startTime = System.currentTimeMillis();
+        log.info("开始初始化帧抓取器");
+
+        // 初始化帧录制器，例如数据结构（音频流、视频流指针，编码器），
+        // 调用av_guess_format方法，确定视频输出时的封装方式，
+        // 媒体上下文对象的内存分配，
+        // 编码器的各项参数设置
         recorder.start();
 
+        log.info("帧录制初始化完成，耗时[{}]毫秒", System.currentTimeMillis()-startTime);
+
         Frame frame;
+
+        startTime = System.currentTimeMillis();
+        log.info("开始推流");
+
+        long videoTS = 0;
+
+        int videoFrameNum = 0;
+        int audioFrameNum = 0;
+        int dataFrameNum = 0;
+
+        // 持续从视频源取帧
         while (null!=(frame=grabber.grab())) {
+            videoTS = 1000 * (System.currentTimeMillis() - startTime);
+
+            recorder.setTimestamp(videoTS);
+
+            if (null!=frame.image) {
+                videoFrameNum++;
+            }
+
+            if (null!=frame.samples) {
+                audioFrameNum++;
+            }
+
+            if (null!=frame.data) {
+                dataFrameNum++;
+            }
+
+            // 取出的每一帧，都推送到SRS
             recorder.record(frame);
         }
 
+        log.info("推送完成，视频帧[{}]，音频帧[{}]，数据帧[{}]，耗时[{}]秒",
+                videoFrameNum,
+                audioFrameNum,
+                dataFrameNum,
+                (System.currentTimeMillis()-startTime)/1000);
+
+        // 关闭帧录制器
         recorder.close();
+        // 关闭帧抓取器
         grabber.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        for(;;) {
+            grabAndPush(MP4_FILE_PATH, SRS_PUSH_ADDRESS);
+        }
     }
 }
