065ab0bb85bcae9dc36776676cdc6331b0ca79bd
zq2599 <zq2599@gmail.com>
2022-02-07 21:38:23 +0800

    增加脚本

--- nginx-clojure-tutorials/handler-demo/src/main/java/com/bolingcavalry/handlerdemo/BasicAuthHandler.java
+++ nginx-clojure-tutorials/handler-demo/src/main/java/com/bolingcavalry/handlerdemo/BasicAuthHandler.java
@@ -21,14 +21,23 @@ public  class BasicAuthHandler implements NginxJavaRingHandler {
     public Object[] invoke(Map<String, Object> request) {
         // 从header中获取authorization字段
         String auth = (String) ((Map)request.get(HEADERS)).get("authorization");
+
+        // 如果header中没有authorization，就返回401错误，并带上body
         if (auth == null) {
             return new Object[] { 401, ArrayMap.create("www-authenticate", "Basic realm=\"Secure Area\""),
                     "<HTML><BODY><H1>401 Unauthorized.</H1></BODY></HTML>" };
         }
+
+        // authorization应该是 : Basic xfeep:hello!，所以这里先将"Basic "去掉，然后再用":"分割
         String[] up = new String(DatatypeConverter.parseBase64Binary(auth.substring("Basic ".length())), DEFAULT_ENCODING).split(":");
+
+        // 只是为了演示用是，所以账号和密码的检查逻辑在代码中是写死的，
+        // 如果账号等于"xfeep"，并且密码等于"hello!"，就返回PHASE_DONE，这样nginx-clojure就会继续执行后面的content handler
         if (up[0].equals("xfeep") && up[1].equals("hello!")) {
             return PHASE_DONE;
         }
+
+        // 如果账号密码校验不过，就返回401，body内容是提示账号密码不过
         return new Object[] { 401, ArrayMap.create("www-authenticate", "Basic realm=\"Secure Area\""),
                 "<HTML><BODY><H1>401 Unauthorized BAD USER & PASSWORD.</H1></BODY></HTML>" };
     }
