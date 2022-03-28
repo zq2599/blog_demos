7ef4a7d8abb293a560c5d76e5f661ee44bee866d
zq2599 <zq2599@gmail.com>
2022-03-28 08:39:52 +0800

    增加quarkus的事件demo

---
+++ quarkus-tutorials/basic-di/src/main/java/com/bolingcavalry/event/Test.java
@@ -0,0 +1,10 @@
+package com.bolingcavalry.event;
+
+/**
+ * @author will
+ * @email zq2599@gmail.com
+ * @date 2022/3/28 08:32
+ * @description 功能介绍
+ */
+public class Test {
+}
---
+++ quarkus-tutorials/basic-di/src/main/java/com/bolingcavalry/event/bean/MyEvent.java
@@ -0,0 +1,42 @@
+package com.bolingcavalry.event.bean;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author will
+ * @email zq2599@gmail.com
+ * @date 2022/3/28 08:33
+ * @description 事件定义
+ */
+public class MyEvent {
+    /**
+     * 事件源
+     */
+    private String source;
+
+    /**
+     * 事件被消费的总次数
+     */
+    private AtomicInteger consumeNum;
+
+    public MyEvent(String source) {
+        this.source = source;
+        consumeNum = new AtomicInteger();
+    }
+
+    /**
+     * 事件被消费次数加一
+     * @return
+     */
+    public int addNum() {
+        return consumeNum.incrementAndGet();
+    }
+
+    /**
+     * 获取事件被消费次数
+     * @return
+     */
+    public int getNum() {
+        return consumeNum.get();
+    }
+}
