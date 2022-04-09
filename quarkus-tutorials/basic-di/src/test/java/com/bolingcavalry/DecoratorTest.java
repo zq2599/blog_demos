97d1be2bd02608357d2d820e4bd0735048f99482
zq2599 <zq2599@gmail.com>
2022-04-09 21:36:44 +0800

    add decorator demo and test

--- quarkus-tutorials/basic-di/src/test/java/com/bolingcavalry/DecoratorTest.java
+++ quarkus-tutorials/basic-di/src/test/java/com/bolingcavalry/DecoratorTest.java
@@ -1,9 +1,6 @@
 package com.bolingcavalry;
 
 import com.bolingcavalry.decorator.Coffee;
-import com.bolingcavalry.event.producer.MyProducer;
-import com.bolingcavalry.event.producer.TwoChannelWithSingleEvent;
-import com.bolingcavalry.event.producer.TwoChannelWithTwoEvent;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
