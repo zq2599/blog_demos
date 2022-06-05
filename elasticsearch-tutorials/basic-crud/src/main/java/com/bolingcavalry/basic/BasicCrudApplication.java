69c083bfdc62b37748d330c9a9ba8967dd62f5a4
zq2599 <zq2599@gmail.com>
2022-06-05 16:09:02 +0800

    增加es体验的工程

---
+++ elasticsearch-tutorials/basic-crud/src/main/java/com/bolingcavalry/basic/BasicCrudApplication.java
@@ -0,0 +1,18 @@
+package com.bolingcavalry.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zq2599@gmail.com
+ * @Title:
+ * @Package
+ * @Description: web应用启动类
+ * @date 6/5/22 5:17 PM
+ */
+@SpringBootApplication
+public class BasicCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCrudApplication.class, args);
+    }
+}
