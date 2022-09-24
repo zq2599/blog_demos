62f18add782720bfc320fac04ff3958e8cbb52d5
zq2599 <zq2599@gmail.com>
2022-09-25 00:08:25 +0800

    增加leetcode的demo

---
+++ leetcode/src/practice/L0038.java
@@ -0,0 +1,18 @@
+package practice;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: za2599@gmail.com
+ * @create: 2022-06-30 22:33
+ **/
+public class L0038 {
+
+    public String countAndSay(int n) {
+        return "123";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new L0038().countAndSay(3));
+    }
+}
