7797dde78ce044cb291f5c68fa60e012fc79ece4
zq2599 <zq2599@gmail.com>
2023-02-13 08:39:23 +0800

    增加GORM的demo

---
+++ tutorials/gorm-tutorials/go.mod
@@ -0,0 +1,11 @@
+module gorm-tutorials
+
+go 1.19
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	gorm.io/driver/sqlite v1.4.4 // indirect
+	gorm.io/gorm v1.24.5 // indirect
+)
---
+++ tutorials/gorm-tutorials/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+const (
+	FIRST_PRODUCT_CODE = "D101"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: FIRST_PRODUCT_CODE, Price: 1000})
+
+	var product Product
+
+	db.First(&product, "code = ?", FIRST_PRODUCT_CODE)
+
+	fmt.Printf("product: %+v\n", product)
+
+	db.Delete(&product, product.ID)
+}
