57265f387984df5bbc492abf47fc968c604c7775
zq2599 <zq2599@gmail.com>
2023-06-30 12:51:54 +0000

    feat: 完善client-go的单元测试代码

--- tutorials/client-go-unit-tutorials/handler/basic_crud_test.go
+++ tutorials/client-go-unit-tutorials/handler/basic_crud_test.go
@@ -4,7 +4,9 @@ import (
 	"client-go-unit-tutorials/handler"
 	initrouter "client-go-unit-tutorials/init_router"
 	kubernetesservice "client-go-unit-tutorials/kubernetes_service"
+	"client-go-unit-tutorials/unitfake"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,6 +18,20 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
 )
+
+const (
+	TEST_NAMESPACE       = "client-go-tutorial"
+	TEST_POD_NAME_PREFIX = "nginx-pod-"
+	TEST_IMAGE           = "nginx:latest"
+	TEST_LABEL_APP       = "nginx-app"
+	TEST_POD_NUM         = 3
+)
+
+// 用于保存web响应的body
+type ResponsePodNames struct {
+	Message string   `json:"message"`
+	Pods    []string `json:"pods"`
+}
 
 // 1. 定义suite数据结构
 type MySuite struct {
@@ -34,6 +50,14 @@ func (mySuite *MySuite) SetupTest() {
 	mySuite.ctx, mySuite.cancel = context.WithCancel(context.Background())
 	mySuite.clientSet = client
 	mySuite.router = initrouter.InitRouter()
+
+	// 初始化数据，创建namespace
+	if err := unitfake.CreateNamespace(mySuite.ctx, client, TEST_NAMESPACE); err != nil {
+		log.Fatalf("create namespace error, %s", err.Error())
+	}
+
+	// 初始化数据，创建pod
+	createPod(mySuite.ctx, client, 3)
 }
 
 // 3. 定义结束
@@ -57,9 +81,9 @@ func (mySuite *MySuite) TestBasicCrud() {
 		url := fmt.Sprintf("%s?%s=%s&%s=%s",
 			initrouter.PATH_QUERY_PODS_BY_LABEL_APP,
 			handler.PARAM_NAMESPACE,
-			"client-go-tutorial",
+			TEST_NAMESPACE,
 			handler.PARAM_APP,
-			"nginx-app")
+			TEST_LABEL_APP)
 
 		code, body, error := singleTest(mySuite, url)
 
@@ -70,6 +94,9 @@ func (mySuite *MySuite) TestBasicCrud() {
 
 		// 检查结果
 		mySuite.EqualValues(http.StatusOK, code)
+
+		//
+		check(&mySuite.Suite, body, TEST_POD_NUM)
 
 		log.Printf("response : %s", body)
 	})
@@ -82,4 +109,32 @@ func singleTest(mySuite *MySuite, url string) (int, string, error) {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	mySuite.router.ServeHTTP(w, req)
 	return w.Code, w.Body.String(), nil
+}
+
+// 7. 辅助方法，用于创建多个pod
+func createPod(context context.Context, client kubernetes.Interface, num int) {
+	for i := 0; i < num; i++ {
+		if err := unitfake.CreatePods(context,
+			client,
+			TEST_NAMESPACE,
+			fmt.Sprintf("%s%d", TEST_POD_NAME_PREFIX, i),
+			TEST_IMAGE,
+			TEST_LABEL_APP); err != nil {
+			log.Fatalf("create pod [%d] error, %s", i, err.Error())
+		}
+	}
+}
+
+// 8. 辅助方法，解析web响应，检查结果是否符合预期
+func check(suite *suite.Suite, body string, expectNum int) {
+	suite.NotNil(body)
+	response := &ResponsePodNames{}
+
+	err := json.Unmarshal([]byte(body), response)
+
+	if err != nil {
+		log.Fatalf("unmarshal response error, %s", err.Error())
+	}
+
+	suite.EqualValues(expectNum, len(response.Pods))
 }
