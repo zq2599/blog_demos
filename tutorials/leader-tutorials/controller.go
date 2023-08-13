69d3e4f74a19252a75d375ea667a531d11a3be13
zq2599 <zq2599@gmail.com>
2023-08-13 07:43:51 +0000

    feat:删除无用的文件，完善demo

--- tutorials/leader-tutorials/controller.go
+++ tutorials/leader-tutorials/controller.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"k8s.io/klog/v2"
 
-	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	objectruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 )
 
+const (
+	LABLE_SERVICE_UPDATE_TIME = "service-update-time" // 这个label用来记录service的更新时间
+)
+
 // 自定义controller数据结构，嵌入了真实的控制器
 type Controller struct {
+	ctx       context.Context
+	clientset *kubernetes.Clientset
 	// 本地缓存，关注的对象都会同步到这里
 	indexer cache.Indexer
 	// 消息队列，用来触发对真实对象的处理事件
@@ -38,7 +48,7 @@ func (c *Controller) processNextItem() bool {
 	defer c.queue.Done(key)
 
 	// 注意，这里的syncToStdout应该是业务代码，处理对象变化的事件
-	err := c.syncToStdout(key.(string))
+	err := c.updatePodsLabel(key.(string))
 
 	// 如果前面的业务逻辑遇到了错误，就在此处理
 	c.handleErr(err, key)
@@ -53,45 +63,66 @@ func (c *Controller) runWorker() {
 	}
 }
 
-// syncToStdout 这是业务逻辑代码，被调用意味着key对应的对象有变化(新增或者修改)
-func (c *Controller) syncToStdout(key string) error {
+// updatePodsLabel 这是业务逻辑代码，被调用意味着key对应的对象有变化(新增或者修改)
+func (c *Controller) updatePodsLabel(key string) error {
+	// 开始进入controller的业务逻辑
+	klog.Infof("[%s]这里是controller的业务逻辑，key [%s]", processIndentify, key)
 	// 从本地缓存中取出完整的对象
-	obj, exists, err := c.indexer.GetByKey(key)
+	_, exists, err := c.indexer.GetByKey(key)
 	if err != nil {
-		klog.Errorf("Fetching object with key %s from store failed with %v", key, err)
+		klog.Errorf("[%s]根据key[%s]从本地缓存获取对象失败 : %v", processIndentify, key, err)
 		return err
 	}
 
-	// 如果不存在，就表示这是个删除事件
 	if !exists {
-		fmt.Printf("Pod %s does not exist anymore\n", key)
+		klog.Infof("[%s]对象不存在，key [%s]，这是个删除事件", processIndentify, key)
 	} else {
-		// 这里无视了obj具体是什么类型的对象(deployment、pod这些都有可能)，
-		// 用meta.Accessor转换出metav1.Object对象后就能获取该对象的所有meta信息
-		objMeta, err := meta.Accessor(obj)
+		klog.Infof("[%s]对象存在，key [%s]，这是个新增或修改事件", processIndentify, key)
+	}
 
+	// 代码走到这里，表示监听的对象发生了变化，
+	// 按照业务设定，需要修改pod的指定label,
+	// 准备好操作pod的接口
+	podInterface := c.clientset.CoreV1().Pods(NAMESPACE)
+
+	// 远程取得最新的pod列表
+	pods, err := podInterface.List(c.ctx, metav1.ListOptions{})
+
+	if err != nil {
+		klog.Errorf("[%s]远程获取pod列表失败 : %v", processIndentify, err)
+		return err
+	}
+
+	// 将service的变化时间写入pod的指定label，这里先获取当前时间
+	updateTime := time.Now().Format("20060102150405")
+	// 准备patch对象
+	patchData := map[string]interface{}{
+		"metadata": map[string]interface{}{
+			"labels": map[string]interface{}{
+				LABLE_SERVICE_UPDATE_TIME: updateTime,
+			},
+		},
+	}
+
+	// 转为byte数组，稍后更新pod的时候，就用这个数组进行patch更新
+	patchByte, _ := json.Marshal(patchData)
+
+	// 遍历所有pod，逐个更新label
+	for _, pod := range pods.Items {
+		podName := pod.Name
+		klog.Infof("[%s]正在更新pod [%s]", processIndentify, podName)
+
+		_, err := podInterface.Patch(c.ctx, podName, types.MergePatchType, patchByte, metav1.PatchOptions{})
+
+		// 失败就返回，会导致整体重试
 		if err != nil {
-			klog.Errorf("get meta accessor error, [%s], failed with %v", key, err)
+			klog.Infof("[%s]更新pod [%s]失败, %v", processIndentify, podName, err)
 			return err
 		}
 
-		// 取得资源的所有属性
-		labels := objMeta.GetLabels()
-
-		if labels == nil {
-			klog.Infof("name [%s], namespace [%s], label is empty", objMeta.GetName(), objMeta.GetNamespace())
-			return nil
-		}
-
-		// 遍历每个属性，打印出来
-		for key, value := range labels {
-			klog.Infof("name [%s], namespace [%s], key [%s], value [%s]",
-				objMeta.GetName(),
-				objMeta.GetNamespace(),
-				key,
-				value)
-		}
+		klog.Infof("[%s]更新pod [%s]成功", processIndentify, podName)
 	}
+
 	return nil
 }
 
@@ -149,9 +180,9 @@ func (c *Controller) Run(threadiness int, stopCh chan struct{}) {
 }
 
 // CreateAndStartController 为了便于外部使用，这里将controller的创建和启动封装在一起
-func CreateAndStartController(c cache.Getter, objType objectruntime.Object, resource string, namespace string, stopCh chan struct{}) {
+func CreateAndStartController(ctx context.Context, clientset *kubernetes.Clientset, objType objectruntime.Object, resource string, namespace string, stopCh chan struct{}) {
 	// ListWatcher用于获取数据并监听资源的事件
-	podListWatcher := cache.NewListWatchFromClient(c, resource, NAMESPACE, fields.Everything())
+	podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), resource, NAMESPACE, fields.Everything())
 
 	// 限速队列，里面存的是有事件发生的对象的身份信息，而非对象本身
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
@@ -184,9 +215,11 @@ func CreateAndStartController(c cache.Getter, objType objectruntime.Object, reso
 	}, cache.Indexers{})
 
 	controller := &Controller{
-		informer: informer,
-		indexer:  indexer,
-		queue:    queue,
+		ctx:       ctx,
+		clientset: clientset,
+		informer:  informer,
+		indexer:   indexer,
+		queue:     queue,
 	}
 
 	go controller.Run(1, stopCh)
--- tutorials/leader-tutorials/main.go
+++ tutorials/leader-tutorials/main.go
@@ -23,53 +23,12 @@ const (
 	NAMESPACE = "client-go-tutorials"
 )
 
+// 用于表明当前进程身份的全局变量，目前用的是uuid
 var processIndentify string
 
-func startLeaderElection(ctx context.Context, clientset *kubernetes.Clientset, stop chan struct{}) {
-	// 创建锁对象
-	lock := &resourcelock.LeaseLock{
-		LeaseMeta: metav1.ObjectMeta{
-			Name:      "leader-tutorials",
-			Namespace: NAMESPACE,
-		},
-		Client: clientset.CoordinationV1(),
-		LockConfig: resourcelock.ResourceLockConfig{
-			Identity: processIndentify,
-		},
-	}
-
-	// 启动选主操作
-	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
-		Lock:            lock,
-		ReleaseOnCancel: true,
-		LeaseDuration:   10 * time.Second,
-		RenewDeadline:   5 * time.Second,
-		RetryPeriod:     2 * time.Second,
-		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(ctx context.Context) {
-				klog.Infof("Leader election success [%s]", processIndentify)
-				// 就像抢分布式锁一样，当前进程选举成功的时候，这的代码就会被执行，
-				// 所以，在这里填写抢锁成功的业务逻辑吧，本例中就是监听service变化，然后修改pod的label
-				CreateAndStartController(clientset.CoreV1().RESTClient(), &v1.Service{}, "services", NAMESPACE, stop)
-			},
-			OnStoppedLeading: func() {
-				// 失去了leader时的逻辑
-				klog.Infof("leader lost: %s", processIndentify)
-				os.Exit(0)
-			},
-			OnNewLeader: func(identity string) {
-				// 收到通知，知道最终的选举结果
-				if identity == processIndentify {
-					// I just got the lock
-					return
-				}
-				klog.Infof("new leader elected: %s", identity)
-			},
-		},
-	})
-}
-
-func main() {
+// initOrDie client有关的初始化操作
+func initOrDie() *kubernetes.Clientset {
+	klog.Infof("[%s]开始初始化kubernetes客户端相关对象", processIndentify)
 	var kubeconfig *string
 	var master string
 
@@ -94,22 +53,80 @@ func main() {
 	if err != nil {
 		klog.Fatal(err)
 	}
+	klog.Infof("[%s]kubernetes客户端相关对象创建成功", processIndentify)
+	return clientset
+}
 
+// startLeaderElection 选主的核心逻辑代码
+func startLeaderElection(ctx context.Context, clientset *kubernetes.Clientset, stop chan struct{}) {
+	klog.Infof("[%s]创建选主所需的锁对象", processIndentify)
+	// 创建锁对象
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      "leader-tutorials",
+			Namespace: NAMESPACE,
+		},
+		Client: clientset.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: processIndentify,
+		},
+	}
+	klog.Infof("[%s]开始选主", processIndentify)
+	// 启动选主操作
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock:            lock,
+		ReleaseOnCancel: true,
+		LeaseDuration:   10 * time.Second,
+		RenewDeadline:   5 * time.Second,
+		RetryPeriod:     2 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				klog.Infof("[%s]当前进程是leader，只有leader才能执行的业务逻辑立即开始", processIndentify)
+				// 就像抢分布式锁一样，当前进程选举成功的时候，这的代码就会被执行，
+				// 所以，在这里填写抢锁成功的业务逻辑吧，本例中就是监听service变化，然后修改pod的label
+				CreateAndStartController(ctx, clientset, &v1.Service{}, "services", NAMESPACE, stop)
+			},
+			OnStoppedLeading: func() {
+				// 失去了leader时的逻辑
+				klog.Infof("[%s]失去leader身份，不再是leader了", processIndentify)
+				os.Exit(0)
+			},
+			OnNewLeader: func(identity string) {
+				// 收到通知，知道最终的选举结果
+				if identity == processIndentify {
+					klog.Infof("[%s]选主结果出来了，当前进程就是leader", processIndentify)
+					// I just got the lock
+					return
+				}
+				klog.Infof("[%s]选主结果出来了，leader是 : [%s]", processIndentify, identity)
+			},
+		},
+	})
+}
+
+func main() {
+	// 一次性确定当前进程身份
+	processIndentify = uuid.New().String()
+
+	// 准备一个带cancel的context，这样在主程序退出的时候，可以将停止的信号传递给业务
 	ctx, cancel := context.WithCancel(context.Background())
-
+	// 这个是用来停止controller的
 	stop := make(chan struct{})
 
+	// 主程序结束的时候，下面的操作可以将业务逻辑都停掉
 	defer func() {
 		close(stop)
 		cancel()
 	}()
 
-	processIndentify = uuid.New().String()
+	// 初始化clientSet配置，因为是启动阶段，所以必须初始化成功，否则进程退出
+	clientset := initOrDie()
 
+	// 在一个新的协程中执行选主逻辑，以及选主成功的后的逻辑
 	go startLeaderElection(ctx, clientset, stop)
 
 	// 这里可以继续做其他事情
-	klog.Infof("other business will be execute here [%s]", processIndentify)
+	klog.Infof("选主的协程已经在运行，接下来可以执行其他业务 [%s]", processIndentify)
 
 	select {}
 }
