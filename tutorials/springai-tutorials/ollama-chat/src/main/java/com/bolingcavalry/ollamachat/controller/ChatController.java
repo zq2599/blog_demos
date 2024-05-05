4ea0c9de3ae62011a77c833629fc25a86a6c2171
zq2599 <zq2599@gmail.com>
2024-05-06 00:00:50 +0800

    feat: 增加ollama-chat的工程

---
+++ tutorials/springai-tutorials/ollama-chat/src/main/java/com/bolingcavalry/ollamachat/controller/ChatController.java
@@ -0,0 +1,34 @@
+package com.bolingcavalry.ollamachat.controller;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.messages.UserMessage;
+
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final OllamaChatClient chatClient;
+
+    public ChatController(OllamaChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+	public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
+    }
+}
